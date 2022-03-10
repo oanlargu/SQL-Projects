5f870ff5f5c6352f5702278d55fc2890fbfb5aaa
Onur Anlargu <80003879+oanlargu@users.noreply.github.com>
2022-03-10 19:40:52 +0300

    Update Prime Numbers.sql

--- 02_HackerRank/Prime Numbers.sql
+++ 02_HackerRank/Prime Numbers.sql
@@ -1,26 +1,27 @@
 DECLARE @n INT=3
 DECLARE @d INT
 DECLARE @p INT
-DECLARE @primes VARCHAR (1000)=''
+DECLARE @primes VARCHAR (8000)='2'
+DECLARE @is_prime VARCHAR =''
 
 WHILE @n <= 1000
 	BEGIN
 		SET @d = 2
-
-		WHILE @d <= 1000
+			WHILE @d <= 1000
 			BEGIN
-				IF @d = @n SET @d = @d + 1
-				ELSE
-					BEGIN
-						IF @n % @d = 0	BREAK
-						ELSE SET @d = @d + 1
+			SET @is_prime ='T'
+				IF @n <> @d AND @n % @d = 0
+					BEGIN 
+						SET @is_prime ='F'
+						BREAK
 					END
-			
-			SET @primes = @primes + CAST(@n AS varchar) + '&'
+			SET @d = @d + 1
 			END
-	
-	SET @n = @n + 1
+	IF @is_prime = 'T' 
+		BEGIN
+			SET @primes = @primes + '&' + CAST(@n AS varchar) 
+		END
+	SET @n = @n+1
 	END
 
 PRINT @primes
-
